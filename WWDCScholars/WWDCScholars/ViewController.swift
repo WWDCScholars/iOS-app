78a56de10e3022f5c1b2bcb2d364318f9e9ff5a1
Sam0711er <Sam.Eckert00@icloud.com>
2016-02-27 22:58:03 +0100

    Added the Xcodeproj

---
+++ WWDCScholars/WWDCScholars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WWDCScholars
+//
+//  Created by Sam Eckert on 27.02.16.
+//  Copyright © 2016 WWDCScholars. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

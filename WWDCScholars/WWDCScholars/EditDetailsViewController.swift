f9e4508609b8fc8931a85b57f356cf6491d44f8e
Sam0711er <Sam.Eckert00@icloud.com>
2016-04-10 16:57:29 +0200

    Added EditVC (V1) more details in description
    -Added EditVC & fields for name, age, city & country, short bio, yt &
    github link for apps & profile picture, everything will be put on a
    UIScrollView later
    -Added youTube & GitHub Small Icon Files
    -Accessible via Credits > Scholars Access for now, we’ll put the
    password check before that in the future

---
+++ WWDCScholars/WWDCScholars/EditDetailsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  EditDetailsViewController.swift
+//  WWDCScholars
+//
+//  Created by Sam Eckert on 10.04.16.
+//  Copyright © 2016 WWDCScholars. All rights reserved.
+//
+
+import UIKit
+
+class EditDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        viewSetup()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    func viewSetup(){
+        self.title = "Edit your details"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    @IBAction func doneAction(sender: AnyObject) {
+        // Save data here, then dismiss the view
+
+        // +++ Data Saving needs to be implemented here +++
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    @IBAction func cancelAction(sender: AnyObject) {
+        // Dismiss the view
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
---
+++ WWDCScholars/WWDCScholars/FloatLabelTextView.swift
@@ -0,0 +1,217 @@
+//
+//  FloatLabelTextView.swift
+//  FloatLabelFields
+//
+//  Created by Fahim Farook on 28/11/14.
+//  Copyright (c) 2014 RookSoft Ltd. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class FloatLabelTextView: UITextView {
+	let animationDuration = 0.3
+	let placeholderTextColor = UIColor.lightGrayColor().colorWithAlphaComponent(0.65)
+	private var isIB = false
+	private var title = UILabel()
+	private var hintLabel = UILabel()
+	private var initialTopInset:CGFloat = 0
+	
+	// MARK:- Properties
+	override var accessibilityLabel:String! {
+		get {
+			if text.isEmpty {
+				return title.text!
+			} else {
+				return text
+			}
+		}
+		set {
+		}
+	}
+	
+	var titleFont:UIFont = UIFont.systemFontOfSize(12.0) {
+		didSet {
+			title.font = titleFont
+		}
+	}
+	
+	@IBInspectable var hint:String = "" {
+		didSet {
+			title.text = hint
+			title.sizeToFit()
+			var r = title.frame
+			r.size.width = frame.size.width
+			title.frame = r
+			hintLabel.text = hint
+			hintLabel.sizeToFit()
+		}
+	}
+	
+	@IBInspectable var hintYPadding:CGFloat = 0.0 {
+		didSet {
+			adjustTopTextInset()
+		}
+	}
+	
+	@IBInspectable var titleYPadding:CGFloat = 0.0 {
+		didSet {
+			var r = title.frame
+			r.origin.y = titleYPadding
+			title.frame = r
+		}
+	}
+	
+	@IBInspectable var titleTextColour:UIColor = UIColor.grayColor() {
+		didSet {
+			if !isFirstResponder() {
+				title.textColor = titleTextColour
+			}
+		}
+	}
+	
+	@IBInspectable var titleActiveTextColour:UIColor = UIColor.cyanColor() {
+		didSet {
+			if isFirstResponder() {
+				title.textColor = titleActiveTextColour
+			}
+		}
+	}
+	
+	// MARK:- Init
+	required init(coder aDecoder:NSCoder) {
+		super.init(coder:aDecoder)!
+		setup()
+	}
+	
+	override init(frame:CGRect, textContainer:NSTextContainer?) {
+		super.init(frame:frame, textContainer:textContainer)
+		setup()
+	}
+	
+	deinit {
+		if !isIB {
+			let nc = NSNotificationCenter.defaultCenter()
+			nc.removeObserver(self, name:UITextViewTextDidChangeNotification, object:self)
+			nc.removeObserver(self, name:UITextViewTextDidBeginEditingNotification, object:self)
+			nc.removeObserver(self, name:UITextViewTextDidEndEditingNotification, object:self)
+		}
+	}
+	
+	// MARK:- Overrides
+	override func prepareForInterfaceBuilder() {
+		isIB = true
+		setup()
+	}
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		adjustTopTextInset()
+		hintLabel.alpha = text.isEmpty ? 1.0 : 0.0
+		let r = textRect()
+		hintLabel.frame = CGRect(x:r.origin.x, y:r.origin.y, width:hintLabel.frame.size.width, height:hintLabel.frame.size.height)
+		setTitlePositionForTextAlignment()
+		let isResp = isFirstResponder()
+		if isResp && !text.isEmpty {
+			title.textColor = titleActiveTextColour
+		} else {
+			title.textColor = titleTextColour
+		}
+		// Should we show or hide the title label?
+		if text.isEmpty {
+			// Hide
+			hideTitle(isResp)
+		} else {
+			// Show
+			showTitle(isResp)
+		}
+	}
+	
+	// MARK:- Private Methods
+	private func setup() {
+		initialTopInset = textContainerInset.top
+		textContainer.lineFragmentPadding = 0.0
+		titleActiveTextColour = tintColor
+		// Placeholder label
+		hintLabel.font = font
+		hintLabel.text = hint
+		hintLabel.numberOfLines = 0
+		hintLabel.lineBreakMode = NSLineBreakMode.ByWordWrapping
+		hintLabel.backgroundColor = UIColor.clearColor()
+		hintLabel.textColor = placeholderTextColor
+		insertSubview(hintLabel, atIndex:0)
+		// Set up title label
+		title.alpha = 0.0
+		title.font = titleFont
+		title.textColor = titleTextColour
+		title.backgroundColor = backgroundColor
+		if !hint.isEmpty {
+			title.text = hint
+			title.sizeToFit()
+		}
+		self.addSubview(title)
+        
+        self.contentInset = UIEdgeInsetsMake(0, 10, 0, 10)
+        
+        
+		// Observers
+		if !isIB {
+			let nc = NSNotificationCenter.defaultCenter()
+			nc.addObserver(self, selector:#selector(UIView.layoutSubviews), name:UITextViewTextDidChangeNotification, object:self)
+			nc.addObserver(self, selector:#selector(UIView.layoutSubviews), name:UITextViewTextDidBeginEditingNotification, object:self)
+			nc.addObserver(self, selector:#selector(UIView.layoutSubviews), name:UITextViewTextDidEndEditingNotification, object:self)
+		}
+	}
+
+	private func adjustTopTextInset() {
+		var inset = textContainerInset
+		inset.top = initialTopInset + title.font.lineHeight + hintYPadding
+		textContainerInset = inset
+	}
+	
+	private func textRect()->CGRect {
+		var r = UIEdgeInsetsInsetRect(bounds, contentInset)
+		r.origin.x += textContainer.lineFragmentPadding
+		r.origin.y += textContainerInset.top
+		return CGRectIntegral(r)
+	}
+	
+	private func setTitlePositionForTextAlignment() {
+		var titleLabelX = textRect().origin.x
+		var placeholderX = titleLabelX
+		if textAlignment == NSTextAlignment.Center {
+			titleLabelX = (frame.size.width - title.frame.size.width) * 0.5
+			placeholderX = (frame.size.width - hintLabel.frame.size.width) * 0.5
+		} else if textAlignment == NSTextAlignment.Right {
+			titleLabelX = frame.size.width - title.frame.size.width
+			placeholderX = frame.size.width - hintLabel.frame.size.width
+		}
+		var r = title.frame
+		r.origin.x = titleLabelX
+		title.frame = r
+		r = hintLabel.frame
+		r.origin.x = placeholderX
+		hintLabel.frame = r
+	}
+	
+	private func showTitle(animated:Bool) {
+		let dur = animated ? animationDuration : 0
+		UIView.animateWithDuration(dur, delay:0, options: [UIViewAnimationOptions.BeginFromCurrentState, UIViewAnimationOptions.CurveEaseOut], animations:{
+			// Animation
+			self.title.alpha = 1.0
+			var r = self.title.frame
+			r.origin.y = self.titleYPadding + self.contentOffset.y
+			self.title.frame = r
+			}, completion:nil)
+	}
+	
+	private func hideTitle(animated:Bool) {
+		let dur = animated ? animationDuration : 0
+		UIView.animateWithDuration(dur, delay:0, options: [UIViewAnimationOptions.BeginFromCurrentState, UIViewAnimationOptions.CurveEaseIn], animations:{
+			// Animation
+			self.title.alpha = 0.0
+			var r = self.title.frame
+			r.origin.y = self.title.font.lineHeight + self.hintYPadding
+			self.title.frame = r
+			}, completion:nil)
+	}
+}

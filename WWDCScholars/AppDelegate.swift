53b9d600b22ca036c6e57673a3b0a23c15078ab1
Matthijs Logemann <matthijs.logemann@xs4all.nl>
2016-04-11 08:46:27 +0200

    Added Fabric and Crashlytics

--- WWDCScholars/AppDelegate.swift
+++ WWDCScholars/AppDelegate.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        Fabric.with([Crashlytics.self])
+
         self.styleUI()
         
         return true

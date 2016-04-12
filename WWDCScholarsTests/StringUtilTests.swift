de9c56c8727badc5ee2174f6543f7b2f167886bd
matthijs2704 <matthijs.logemann@xs4all.nl>
2016-04-12 18:32:42 +0200

    Split up StringUtilTests into smaller tests

--- WWDCScholarsTests/StringUtilTests.swift
+++ WWDCScholarsTests/StringUtilTests.swift
@@ -10,26 +10,32 @@ import XCTest
 
 class StringUtilTests: XCTestCase {
 
-    func testGitHubLink() {
+    func testGitHubLinkInvalid() {
         let nonGitHubLink = "http://google.com/matthijs2704"
         XCTAssertFalse(nonGitHubLink.isValidGitHubLink(), "String isn't a GitHub link, but the validator thinks it is")
-        
+    }
+    
+    func testGitHubLinkValid() {
         let gitHubLink = "http://github.com/matthijs2704"
         XCTAssertTrue(gitHubLink.isValidGitHubLink(), "String is a GitHub link, but the validator thinks it isn't")
     }
     
-    func testFacebookLink() {
+    func testFacebookLinkInvalid() {
         let nonFacebookLink = "http://google.com/matthijs2704"
         XCTAssertFalse(nonFacebookLink.isValidFacebookLink(), "String isn't a Facebook link, but the validator thinks it is")
-        
+    }
+    
+    func testFacebookLinkValid() {
         let facebookLink = "https://www.facebook.com/matthijs.logemann"
         XCTAssertTrue(facebookLink.isValidFacebookLink(), "String is a Facebook link, but the validator thinks it isn't")
     }
     
-    func testTwitterLink() {
+    func testTwitterLinkInvalid() {
         let nonTwitterLink = "http://google.com/matthijs2704"
         XCTAssertFalse(nonTwitterLink.isValidTwitterLink(), "String isn't a Twitter link, but the validator thinks it is")
-
+    }
+    
+    func testTwitterLinkValid() {
         let twitterLink = "http://twitter.com/matthijs2704"
         XCTAssertTrue(twitterLink.isValidTwitterLink(), "String is a Twitter link, but the validator thinks it isn't")
     }

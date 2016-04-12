bf09c1986db814d9c644a9e24ebd09517890c9ed
matthijs2704 <matthijs.logemann@xs4all.nl>
2016-04-12 22:20:54 +0200

    Wrong number in test assert :$

--- WWDCScholarsTests/ScholarAPITests.swift
+++ WWDCScholarsTests/ScholarAPITests.swift
@@ -39,7 +39,7 @@ class ScholarAPITests: XCTestCase {
     }
     
     func testCShowScholars() {
-        XCTAssertEqual(scholarApi.dbManager.scholarCount(), 6)
+        XCTAssertEqual(scholarApi.dbManager.scholarCount(), 5)
     }
     
 }

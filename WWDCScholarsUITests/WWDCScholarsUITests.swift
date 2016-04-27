c5788b787231b24611da6dfde1a630c12acb49b1
Sam0711er <Samraven@olivereckert.com>
2016-04-27 17:47:51 +0200

    Adds other UITest Methods

--- WWDCScholarsUITests/WWDCScholarsUITests.swift
+++ WWDCScholarsUITests/WWDCScholarsUITests.swift
@@ -28,13 +28,61 @@ class WWDCScholarsUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func scholarsDetailScreen() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
+//        let app = XCUIApplication()
+     //   app.otherElements.containingType(.Button, identifier:"wwdcScholarsTabIcon").childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Other).element.tap()
+      //  app.collectionViews.cells.otherElements.containingType(.StaticText, identifier:"Sam").images["placeholder"].tap()
+        
+    }
+    
+    func scholarsScreen() {
+        
         let app = XCUIApplication()
-        app.otherElements.containingType(.Button, identifier:"wwdcScholarsTabIcon").childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Other).element.tap()
-        app.collectionViews.cells.otherElements.containingType(.StaticText, identifier:"Sam").images["placeholder"].tap()
+        app.tabBars.buttons["Scholars"].tap()
+        app.collectionViews.staticTexts["2016"].swipeRight()
+        
+    }
+    
+    func creditsScreen(){
+        
+        let app = XCUIApplication()
+        app.tabBars.buttons["Credits"].tap()
+        app.scrollViews.otherElements.tables.staticTexts["Oliver Binns (London, UK)"].swipeUp()
+        
+    }
+    
+    func blogScreen(){
+        XCUIApplication().tabBars.buttons["Blog"].tap()
+        
+    }
+    
+    func chatScreen(){
+        XCUIApplication().tabBars.buttons["Chat"].tap()
+        
+    }
+    
+    func loginScreen(){
+        let app = XCUIApplication()
+        app.tabBars.buttons["Scholars"].tap()
+        app.navigationBars["Scholars"].buttons["barButtonItemIconAccountFilled"].tap()
+    }
+    
+    func mapScreen(){
+        
+        let app = XCUIApplication()
+        let mapiconButton = app.navigationBars["Scholars"].buttons["mapIcon"]
+        mapiconButton.tap()
+        app.alerts["Allow “WWDCScholars” to access your location while you use the app?"].collectionViews.buttons["Allow"].tap()
+        mapiconButton.tap()
+        
+    }
+    
+    func editDetailsScreen(){
+        
+   //     XCUIApplication().otherElements.containingType(.Button, identifier:"wwdcScholarsTabIcon").childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Other).element.pressForDuration(1.1);
         
     }
     

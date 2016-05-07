8a8b85346be7e831322e22a8b6cadd473c80143d
Andrew Walker <andreww500@live.com>
2016-05-07 23:23:07 +0100

    Added photo upload functionality

--- WWDCScholars/EditProfileTableViewController.swift
+++ WWDCScholars/EditProfileTableViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class EditProfileTableViewController: UITableViewController, UITextFieldDelegate {
+class EditProfileTableViewController: UITableViewController, UITextFieldDelegate, UINavigationControllerDelegate {
     @IBOutlet private weak var screenshotCollectionView: UICollectionView!
+    @IBOutlet private weak var profileImageButton: UIButton!
     
-    let numberOfScreenshots = 4
+    private let numberOfScreenshots = 4
+    private let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
-        let dismissKeyboardRecognizer: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(EditProfileTableViewController.dismissKeyboard))
+        let dismissKeyboardRecognizer = UITapGestureRecognizer(target: self, action: #selector(EditProfileTableViewController.dismissKeyboard))
         self.view.addGestureRecognizer(dismissKeyboardRecognizer)
         
         self.styleUI()
@@ -22,29 +24,37 @@ class EditProfileTableViewController: UITableViewController, UITextFieldDelegate
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         self.view.endEditing(true)
+        
         return false
     }
     
     // MARK: - UI
     
     internal func dismissKeyboard() {
-        view.endEditing(true)
+        self.view.endEditing(true)
     }
     
     private func styleUI() {
         self.title = "Edit Profile"
+        
+        self.imagePicker.delegate = self
+        self.imagePicker.allowsEditing = false
+        
+        self.profileImageButton.imageView!.layer.cornerRadius = self.profileImageButton.frame.width / 2
     }
     
     // MARK: - IBActions
     
-    @IBAction func uploadProfileImageTapped(sender: AnyObject) {
+    @IBAction func uploadProfileImageButtonTapped(sender: AnyObject) {
         let actionSheet = UIAlertController(title: "Upload Profile Image", message: nil, preferredStyle: .ActionSheet)
         
         let uploadAction = UIAlertAction(title: "Import from photo library", style: .Default, handler: { (alert: UIAlertAction!) -> Void in
-            
+            self.imagePicker.sourceType = .PhotoLibrary
+            self.presentViewController(self.imagePicker, animated: true, completion: nil)
         })
         let takePhotoAction = UIAlertAction(title: "Take new photo", style: .Default, handler: { (alert: UIAlertAction!) -> Void in
-            
+            self.imagePicker.sourceType = .Camera
+            self.presentViewController(self.imagePicker, animated: true, completion: nil)
         })
         let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
         
@@ -74,5 +84,18 @@ extension EditProfileTableViewController: UICollectionViewDataSource {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("screenshotUploadCollectionViewCell", forIndexPath: indexPath) as! ScreenshotUploadCollectionViewCell
         
         return cell
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+
+extension EditProfileTableViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.profileImageButton.contentMode = .ScaleAspectFit
+            self.profileImageButton.setImage(pickedImage, forState: .Normal)
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }

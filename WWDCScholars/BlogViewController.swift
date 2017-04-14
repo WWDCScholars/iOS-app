45611a8a0cc54a9dc3f68d7e8810aa8e82bb1da4
Andrew Walker <andreww500@live.com>
2017-04-14 01:26:02 +0100

    Apply global styling and implement basic UI elements

---
+++ WWDCScholars/BlogViewController.swift
@@ -0,0 +1,33 @@
+//
+//  BlogViewController.swift
+//  WWDCScholars
+//
+//  Created by Andrew Walker on 14/04/2017.
+//  Copyright © 2017 Andrew Walker. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+internal final class BlogViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    internal override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.styleUI()
+        self.configureUI()
+    }
+    
+    // MARK: - UI
+    
+    private func styleUI() {
+        
+    }
+    
+    private func configureUI() {
+        self.title = "Blog"
+    }
+    
+}
---
+++ WWDCScholars/TeamViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TeamViewController.swift
+//  WWDCScholars
+//
+//  Created by Andrew Walker on 14/04/2017.
+//  Copyright © 2017 Andrew Walker. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+internal final class TeamViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    internal override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.styleUI()
+        self.configureUI()
+    }
+    
+    // MARK: - UI
+    
+    private func styleUI() {
+        
+    }
+    
+    private func configureUI() {
+        self.title = "Team"
+    }
+    
+}

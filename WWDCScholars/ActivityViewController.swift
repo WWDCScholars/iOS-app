7e4c455e9b3953cc643c1158c37dd13489e789d6
matthijs2704 <matthijs.logemann@xs4all.nl>
2017-05-21 22:35:56 +0200

    Implement Activity view with Twitter feed

--- WWDCScholars/ActivityViewController.swift
+++ WWDCScholars/ActivityViewController.swift
@@ -8,9 +8,10 @@
 
 import Foundation
 import UIKit
+import TwitterKit
 
-internal final class ActivityViewController: UIViewController {
-    
+internal final class ActivityViewController: TWTRTimelineViewController {
+
     // MARK: - Lifecycle
     
     internal override func viewDidLoad() {
@@ -18,15 +19,35 @@ internal final class ActivityViewController: UIViewController {
         
         self.styleUI()
         self.configureUI()
+        self.configureTwitterDataSource()
     }
     
     // MARK: - UI
     
     private func styleUI() {
-
+        let composeBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(self.openTweetComposer))
+        self.navigationItem.rightBarButtonItem = composeBarButtonItem
     }
     
     private func configureUI() {
         self.title = "Activity"
+        self.showTweetActions = true
+    }
+    
+    // MARK: - Private functions
+    
+    private func configureTwitterDataSource() {
+        let client = TWTRAPIClient()
+        let query = "#WWDCScholars OR from:@tim_cook OR from:@cue OR from:@jgeleynse OR from:@pschiller OR from:@AngelaAhrendts OR from:@EEhare"
+        let dataSource = TWTRSearchTimelineDataSource(searchQuery: query, apiClient: client)
+        self.dataSource = dataSource
+    }
+    
+    // MARK: - Actions
+    
+    internal func openTweetComposer() {
+        let composer = TWTRComposer()
+        composer.setText("#WWDCScholars")
+        composer.show(from: self) { _ in }
     }
 }

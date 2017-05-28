bd4aef390035a4cdfd179db3ed29ac79f0345505
Andrew Walker <andreww500@live.com>
2017-05-28 17:38:01 +0100

    Add Remaining Social Media Icons

--- WWDCScholars/ProfileSocialAccountsFactory.swift
+++ WWDCScholars/ProfileSocialAccountsFactory.swift
@@ -11,32 +11,82 @@ import UIKit
 
 internal final class ProfileSocialAccountsFactory {
     
+    // MARK: - Private Properties
+    
+    private let scholar: ExampleScholar
+    
     // MARK: - Lifecycle
     
-    private init() {}
+    internal init(scholar: ExampleScholar) {
+        self.scholar = scholar
+    }
     
     // MARK: - Internal Functions
     
-    internal static func accountButtons() -> [UIButton] {
-        let buttons = [self.facebookButton(), self.twitterButton()]
-        return buttons
+    internal func accountButtons() -> [SocialAccountButton] {
+        let buttons = [self.facebookButton(), self.twitterButton(), self.linkedInButton(), self.websiteButton(), self.gitHubButton(), self.iMessageButton()]
+        let visibleButtons = buttons.flatMap({ ($0 as? SocialAccountButton) })
+        
+        return visibleButtons
     }
     
     // MARK: - Private Functions
     
-    private static func facebookButton() -> UIButton {
-        return self.accountButton(withImage: "facebook")
+    private func iMessageButton() -> UIButton? {
+        if let iMessage = self.scholar.socialMedia.iMessage, !iMessage.isEmpty {
+            return self.accountButton(withImageName: "iMessage", url: iMessage)
+        }
+        
+        return nil
     }
     
-    private static func twitterButton() -> UIButton {
-        return self.accountButton(withImage: "twitter")
+    private func gitHubButton() -> UIButton? {
+        if let gitHubURL = self.scholar.socialMedia.gitHub, !gitHubURL.isEmpty {
+            return self.accountButton(withImageName: "gitHub", url: gitHubURL)
+        }
+        
+        return nil
     }
     
-    private static func accountButton(withImage named: String) -> UIButton {
-        let button = UIButton()
-        let image = UIImage(named: named)
+    private func websiteButton() -> UIButton? {
+        if let websiteURL = self.scholar.socialMedia.website, !websiteURL.isEmpty {
+            return self.accountButton(withImageName: "website", url: websiteURL)
+        }
+        
+        return nil
+    }
+    
+    private func linkedInButton() -> UIButton? {
+        if let linkedInURL = self.scholar.socialMedia.linkedIn, !linkedInURL.isEmpty {
+            return self.accountButton(withImageName: "linkedIn", url: linkedInURL)
+        }
+        
+        return nil
+    }
+    
+    private func facebookButton() -> UIButton? {
+        if let facebookURL = self.scholar.socialMedia.facebook, !facebookURL.isEmpty {
+            return self.accountButton(withImageName: "facebook", url: facebookURL)
+        }
+        
+        return nil
+    }
+    
+    private func twitterButton() -> UIButton? {
+        if let twitterURL = self.scholar.socialMedia.twitter, !twitterURL.isEmpty {
+            return self.accountButton(withImageName: "twitter", url: twitterURL)
+        }
+        
+        return nil
+    }
+    
+    private func accountButton(withImageName name: String, url: String) -> SocialAccountButton {
+        let button = SocialAccountButton()
+        let image = UIImage(named: name)
+        button.accountDetail = url
         button.setBackgroundImage(image, for: .normal)
-        button.widthAnchor.constraint(equalToConstant: 35.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 30.0).isActive = true
+        button.isUserInteractionEnabled = false
         return button
     }
 }
---
+++ WWDCScholars/SocialAccountButton.swift
@@ -0,0 +1,27 @@
+//
+//  SocialAccountButton.swift
+//  WWDCScholars
+//
+//  Created by Andrew Walker on 28/05/2017.
+//  Copyright © 2017 Andrew Walker. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+internal final class SocialAccountButton: UIButton {
+    
+    // MARK: - Internal Properties
+    
+    internal var accountDetail: String?
+    
+    // MARK: - Lifecycle
+    
+    internal override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    internal required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}

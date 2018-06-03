f81613e4213c69099164839992ea40b69d5265d4
Moritz Sternemann <moritz@strnmn.me>
2018-06-03 17:27:15 +0200

    Implemented a more swifty version of BatchInfo, set new default batch info

--- WWDCScholars/BatchInfo.swift
+++ WWDCScholars/BatchInfo.swift
@@ -8,91 +8,34 @@
 
 import Foundation
 
-internal final class BatchKey {
+internal enum BatchInfo: String {
+    
+    // MARK: - Cases
+    
+    case wwdc2013 = "WWDC 2013"
+    case wwdc2014 = "WWDC 2014"
+    case wwdc2015 = "WWDC 2015"
+    case wwdc2016 = "WWDC 2016"
+    case wwdc2017 = "WWDC 2017"
+    case wwdc2018 = "WWDC 2018"
+    case saved = "Saved"
     
     // MARK: - Internal Properties
     
-    internal static let wwdc2013 = "WWDC 2013"
-    internal static let wwdc2014 = "WWDC 2014"
-    internal static let wwdc2015 = "WWDC 2015"
-    internal static let wwdc2016 = "WWDC 2016"
-    internal static let wwdc2017 = "WWDC 2017"
-	internal static let wwdc2018 = "WWDC 2018"
-    internal static let saved = "Saved"
-}
-
-internal protocol BatchInfo: class {
-    var title: String { get }
-    var isDefault: Bool { get set }
-    var recordName: String { get }
-}
-
-internal class BatchInfo2013: BatchInfo {
+    var title: String {
+        return String(rawValue.split(separator: " ").last ?? "")
+    }
     
-    // MARK: - Internal Properties
+    var isDefault: Bool {
+        if self == .wwdc2018 {
+            return true
+        }
+        
+        return false
+    }
     
-    internal let title = "2013"
-    internal let recordName = BatchKey.wwdc2013
+    var recordName: String {
+        return self.rawValue
+    }
     
-    internal var isDefault = false
-}
-
-internal class BatchInfo2014: BatchInfo  {
-    
-    // MARK: - Internal Properties
-    
-    internal let title = "2014"
-    internal let recordName = BatchKey.wwdc2014
-    
-    internal var isDefault = false
-}
-
-internal class BatchInfo2015: BatchInfo  {
-    
-    // MARK: - Internal Properties
-    
-    internal let title = "2015"
-    internal let recordName = BatchKey.wwdc2015
-    
-    internal var isDefault = false
-}
-
-internal class BatchInfo2016: BatchInfo {
-    
-    // MARK: - Internal Properties
-    
-    internal let title = "2016"
-    internal let recordName = BatchKey.wwdc2016
-    
-    internal var isDefault = false
-}
-
-internal class BatchInfo2017: BatchInfo  {
-    
-    // MARK: - Internal Properties
-    
-    internal let title = "2017"
-    internal let recordName = BatchKey.wwdc2017
-    
-    internal var isDefault = true
-}
-
-internal class BatchInfo2018: BatchInfo  {
-	
-	// MARK: - Internal Properties
-	
-	internal let title = "2018"
-	internal let recordName = BatchKey.wwdc2018
-	
-	internal var isDefault = true
-}
-
-internal class BatchInfoSaved: BatchInfo  {
-    
-    // MARK: - Internal Properties
-    
-    internal let title = "Saved"
-    internal let recordName = BatchKey.saved
-    
-    internal var isDefault = false
 }

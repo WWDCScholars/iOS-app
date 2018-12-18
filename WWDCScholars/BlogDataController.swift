10134844342b51753f9bb67ec2c0127764ee3eb6
matthijs2704 <matthijs.logemann@xs4all.nl>
2018-12-18 18:07:24 +0100

    Add BlogDataController protocol

--- WWDCScholars/BlogDataController.swift
+++ WWDCScholars/BlogDataController.swift
@@ -1,5 +1,5 @@
 //
-//  ScholarDataController.swift
+//  BlogDataController.swift
 //  WWDCScholars
 //
 //  Created by Matthijs Logemann on 18/12/2018.
@@ -8,51 +8,39 @@
 
 import Foundation
 
-protocol ScholarDataController {
-    /// Returns all scholars for a given year with a given status
+protocol BlogDataController {
+    /// Returns all blogposts
     ///
-    /// - Parameters:
-    ///   - year: WWDC year to get the scholars for
-    ///   - status: Optional status of the scholars returned
-    /// - Returns: An array of scholars with given year and status
-    func scholars(`for` year: WWDCYear, with status: Scholar.Status?) -> [Scholar]
+    /// - Returns: An array of blog posts
+    func blogPosts() -> [BlogPost]
     
     /// Gets a scholar identified by the id
     ///
-    /// - Parameter id: Identifier of the scholar to retrieve
-    /// - Returns: Returns the scholar with given id or nil
-    func scholar(for id: UUID) -> Scholar?
+    /// - Parameter id: Identifier of the blog post to retrieve
+    /// - Returns: Returns the blogpost with given id or nil
+    func blogPost(for id: UUID) -> BlogPost?
     
-    /// Returns the data for a scholar of a certain year
+    /// Returns the amount of blog posts in the database
     ///
-    /// - Parameters:
-    ///   - year: The year to get the data for
-    ///   - scholar: Scholar to get the data for
-    func scholarData(for year: WWDCYear, scholar: Scholar) -> Batch?
+    /// - Returns: The number of blog posts in the database
+    func countPosts() -> Int
     
-    /// Returns the amount of scholars in the database
-    /// for the given year
+    /// Add blog post to the database
     ///
-    /// - Parameter year: Optional year to count the scholars for
-    /// - Returns: The number of scholars for a given year in the database
-    func countScholars(for year: WWDCYear?) -> Int
+    /// - Parameter post: BlogPost to add
+    func add(_ post: BlogPost)
     
-    /// Add scholar to the database
+    /// Removes blog post from a database
     ///
-    /// - Parameter scholar: Scholar to add
-    func add(_ scholar: Scholar)
+    /// - Parameter post: BlogPost to remove
+    func remove(_ post: BlogPost)
     
-    /// Removes scholar from a database
+    /// Update blog post in database
     ///
-    /// - Parameter scholar: Scholar to remove
-    func remove(_ scholar: Scholar)
-    
-    /// Update scholar in database
-    ///
-    /// - Parameter scholar: Scholar to update
-    func update(_ scholar: Scholar)
+    /// - Parameter post: BlogPost to update
+    func update(_ post: BlogPost)
 }
 
-protocol ScholarIterator: IteratorProtocol where Element == Scholar {
-    init(_ wwdcYear: WWDCYear)
+protocol BlogIterator: IteratorProtocol where Element == BlogPost {
+    init(_ dataController: BlogDataController)
 }

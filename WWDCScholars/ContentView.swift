580b03144e4d5563ef441f5a72a1eda8fb1f7ea1
Moritz Sternemann <opensource@moritzsternemann.de>
2021-03-28 19:54:21 +0200

    Add new project

---
+++ WWDCScholars/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WWDCScholars
+//
+//  Created by Moritz Sternemann on 28.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WWDCScholars/WWDCScholarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDCScholarsApp.swift
+//  WWDCScholars
+//
+//  Created by Moritz Sternemann on 28.03.21.
+//
+
+import SwiftUI
+
+@main
+struct WWDCScholarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
